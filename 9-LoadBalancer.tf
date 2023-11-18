848a7e26e9cafafb5b4b99d65fe0f37def0b6422
A.Harris <144071014+Alharris206@users.noreply.github.com>
2023-11-17 22:51:56 -0800

    Update 9-LoadBalancer.tf
    Route 53 update

--- 9-LoadBalancer.tf
+++ 9-LoadBalancer.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "ninjas_alb" {
   name               = "ninjas-load-balancer"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.ninjas-sg02-LB01.id]
+  security_groups    = [aws_security_group.ninjas-443-sg02-LB01.id]
   subnets            = [
     aws_subnet.public-ap-northeast-3a.id,
     aws_subnet.public-ap-northeast-3b.id,
@@ -25,6 +25,24 @@ resource "aws_lb_listener" "http" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.ninjas_tg.arn
+    target_group_arn = aws_lb_target_group.ninjas-lb_tg.arn
+  }
+}
+
+data "aws_acm_certificate" "cert" {
+  domain = "plumbersportal32.com"
+  most_recent = true
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.ninjas_alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+certificate_arn     = data.aws_acm_certificate.cert.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ninjas-lb_tg.arn
   }
 }
