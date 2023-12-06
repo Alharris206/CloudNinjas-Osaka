5666c47af967d27646435e308ff7d102163f5dd0
A.Harris <144071014+Alharris206@users.noreply.github.com>
2023-12-05 16:13:22 -0800

    Update 14-IAM.tf
    permissions update

--- 14-IAM.tf
+++ 14-IAM.tf
@@ -11,6 +11,14 @@ resource "aws_iam_user_group_membership" "Cloudninjas" {
   ]
 }
 
+resource "aws_iam_group_policy_attachment" "Cloudninjas_rds_full_access" {
+  policy_arn = data.aws_iam_policy.rds_full_access.arn
+  group      = aws_iam_group.Cloudninjas.name
+}
+
+data "aws_iam_policy" "rds_full_access" {
+  arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+}
 resource "aws_iam_user" "NinoBrown" {
   name = "NinoBrown"
 }
@@ -41,6 +49,28 @@ resource "aws_iam_user_group_membership" "KingsofOsaka" {
   ]
 }
 
+resource "aws_iam_policy" "KingsofOsaka_ec2_instance_actions" {
+  name        = "Ninjas_ec2_instance_actions"
+  policy      = data.aws_iam_policy_document.KingsofOsaka_ec2_instance_actions.json
+}
+
+resource "aws_iam_group_policy_attachment" "KingsofOsaka_ec2_instance_actions" {
+  policy_arn = aws_iam_policy.KingsofOsaka_ec2_instance_actions.arn
+  group      = aws_iam_group.KingsofOsaka.name
+}
+
+data "aws_iam_policy_document" "KingsofOsaka_ec2_instance_actions" {
+  statement {
+    actions = [
+      "ec2:StartInstances",
+      "ec2:StopInstances",
+    ]
+
+    resources = [
+      "arn:aws:ec2:*:*:instance/*",
+    ]
+  }
+}
 resource "aws_iam_user" "Blade" {
   name = "Blade"
 }
@@ -73,6 +103,26 @@ resource "aws_iam_user_group_membership" "Prometheus" {
   ]
 }
 
+resource "aws_iam_group_policy" "Prometheuos_policy" {
+  name  = "Prometheus_policy"
+  group = aws_iam_group.Prometheus.name
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
 resource "aws_iam_user" "Hudson" {
   name = "Hudson"
 }
@@ -89,6 +139,26 @@ resource "aws_iam_user_group_membership" "WeylandCorp" {
   groups = [
     aws_iam_group.WeylandCorp.name,
   ]
+}
+
+resource "aws_iam_group_policy" "WeylandCorp_policy" {
+  name  = "WeylandCorp_policy"
+  group = aws_iam_group.WeylandCorp.name
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
 
 resource "aws_iam_user" "David" {
